411f300dd6ab2ac01fe658a29ddbd0c2eeefb9ae
Salman Basmechi <business.salmanbasmechi@gmail.com>
2020-11-26 15:11:41 +0330

    OptionsAttribute added.

---
+++ source/Reguto.Annotations/OptionsAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Reguto.Annotations
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class OptionsAttribute : Attribute
+    {
+        public OptionsAttribute(string section)
+        {
+            Section = section ?? throw new ArgumentNullException(nameof(section));
+        }
+
+        public string Section { get; }
+    }
+}
