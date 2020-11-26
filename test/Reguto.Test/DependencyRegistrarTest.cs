96a3f2b47e79c1abb9b1a485511205833f778314
Salman Basmechi <business.salmanbasmechi@gmail.com>
2020-11-26 16:15:37 +0330

    Test_Services method added.

---
+++ test/Reguto.Test/DependencyRegistrarTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Reguto.Annotations;
+using System.Reflection;
+using Xunit;
+
+namespace Reguto.Test
+{
+    public class DependencyRegistrarTest
+    {
+        private readonly IServiceCollection services = new ServiceCollection();
+        private readonly Assembly assembly;
+
+        public DependencyRegistrarTest()
+        {
+            assembly = GetType().Assembly;
+        }
+
+        [Fact]
+        public void Test_Services()
+        {
+            services.ScanAndRegister(assembly);
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var service = serviceProvider.GetService<IService>();
+            var serviceType = service.GetType();
+
+            Assert.Equal(typeof(Service), serviceType);
+        }
+    }
+
+    public interface IService { }
+
+    [Service]
+    public class Service : IService { }
+}
