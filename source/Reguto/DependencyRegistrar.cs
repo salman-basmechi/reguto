e938ee36c7cb95bccd4d50329896bff09de857b5
Salman Basmechi <business.salmanbasmechi@gmail.com>
2020-11-26 15:23:41 +0330

    DependencyRegistrar added.

---
+++ source/Reguto/DependencyRegistrar.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Reguto.Annotations;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Reguto
+{
+    public static class DependencyRegistrar
+    {
+        public static void ScanAndRegister(this IServiceCollection services)
+        {
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            services.ScanAndRegister(assemblies);
+        }
+
+        public static void ScanAndRegister(this IServiceCollection services, Assembly[] assemblies)
+        {
+            services.ScanAndRegister(assemblies, InjectionMode.Singleton, true);
+            services.ScanAndRegister(assemblies, InjectionMode.Scoped, true);
+            services.ScanAndRegister(assemblies, InjectionMode.Transient, true);
+            services.ScanAndRegister(assemblies, InjectionMode.Singleton, false);
+            services.ScanAndRegister(assemblies, InjectionMode.Scoped, false);
+            services.ScanAndRegister(assemblies, InjectionMode.Transient, false);
+        }
+
+        private static void ScanAndRegister(this IServiceCollection services,
+                                            Assembly[] assemblies,
+                                            InjectionMode injectionMode,
+                                            bool asSelf)
+        {
+            services.Scan(scan =>
+            {
+                var serviceTypeSelector = scan.FromAssemblies(assemblies)
+                                              .AddClasses(classes => classes.Where(type =>
+                                              {
+                                                  var injectionAttributeType = typeof(InjectableAttribute);
+                                                  var customeAttribute = type.GetCustomAttributes()
+                                                                             .FirstOrDefault(c => c.GetType() == injectionAttributeType ||
+                                                                                                  c.GetType().BaseType == injectionAttributeType);
+
+                                                  if (customeAttribute is null)
+                                                  {
+                                                      return false;
+                                                  }
+
+                                                  var injectableAttribute = (InjectableAttribute)customeAttribute;
+
+                                                  return injectableAttribute.AsSelf == asSelf &&
+                                                         injectableAttribute.Mode == injectionMode;
+                                              }));
+
+                var lifeTimeSelector = asSelf ? serviceTypeSelector.AsSelf() : serviceTypeSelector.AsImplementedInterfaces();
+                switch (injectionMode)
+                {
+                    case InjectionMode.Singleton:
+                        {
+                            lifeTimeSelector.WithSingletonLifetime();
+                            break;
+                        }
+                    case InjectionMode.Scoped:
+                        {
+                            lifeTimeSelector.WithScopedLifetime();
+                            break;
+                        }
+                    case InjectionMode.Transient:
+                        {
+                            lifeTimeSelector.WithTransientLifetime();
+                            break;
+                        }
+                }
+            });
+        }
+    }
+}
