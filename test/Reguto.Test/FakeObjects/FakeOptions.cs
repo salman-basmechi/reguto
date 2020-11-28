77007989414c254c1c926c186880d1701f867761
Salman Basmechi <business.salmanbasmechi@gmail.com>
2020-11-28 09:57:42 +0330

    Test_Options_Registration added.

---
+++ test/Reguto.Test/FakeObjects/FakeOptions.cs
@@ -0,0 +1,12 @@
+﻿using Reguto.Annotations;
+
+namespace Reguto.Test.FakeObjects
+{
+    [Options("Fake")]
+    class FakeOptions
+    {
+        public int Id { get; init; }
+
+        public string Secret { get; init; }
+    }
+}
---
+++ test/Reguto.Test/OptionsRegistrarTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Reguto.Test.FakeObjects;
+using System.Reflection;
+using Xunit;
+
+namespace Reguto.Test
+{
+    public class OptionsRegistrarTest
+    {
+        private readonly IServiceCollection services = new ServiceCollection();
+        private readonly IConfiguration configuration;
+        private readonly Assembly assembly;
+
+        public OptionsRegistrarTest()
+        {
+            configuration = new ConfigurationBuilder().AddJsonFile("appSettings.json")
+                                                      .Build();
+
+            assembly = GetType().Assembly;
+        }
+
+        [Fact]
+        public void Test_Options_Registration()
+        {
+            var options = GetFakeOptions();
+
+            Assert.NotNull(options);
+        }
+
+        private IOptions<FakeOptions> GetFakeOptions()
+        {
+            services.ScanAndConfigureOptions(configuration, assembly);
+
+            var serviceProvider = services.BuildServiceProvider();
+            return serviceProvider.GetService<IOptions<FakeOptions>>();
+        }
+    }
+}
